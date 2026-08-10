baac48d69556df09b4a65ee5cca32f36463b2ed3
Bert Tejeda <berttejeda@gmail.com>
2026-08-10 14:43:50 -0400

    Added release workflow and homebrew formula

---
+++ Formula/bert-validator.rb
@@ -0,0 +1,29 @@
+# Homebrew formula for bert.validator
+#
+# To use this formula directly from this repository:
+#   brew tap berttejeda/bert.validator https://github.com/berttejeda/bert.validator.git
+#   brew install berttejeda/bert.validator/bert-validator
+#
+# After pushing a new Git tag, replace the sha256 below with the SHA-256 of the
+# source archive for that tag, e.g.:
+#   curl -sL https://github.com/berttejeda/bert.validator/archive/refs/tags/v1.2.0.tar.gz | shasum -a 256
+
+class BertValidator < Formula
+  desc "YAML-driven script validator"
+  homepage "https://github.com/berttejeda/bert.validator"
+  url "https://github.com/berttejeda/bert.validator/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  head "https://github.com/berttejeda/bert.validator.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-trimpath",
+           "-ldflags", "-s -w -X main.Version=#{version}",
+           "-o", bin/"bert.validator"
+  end
+
+  test do
+    system bin/"bert.validator", "--version"
+  end
+end
