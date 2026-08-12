d029a614856e5d02b9fc58150095f33396fb9e6c
Bert Tejeda <berttejeda@gmail.com>
2026-08-12 09:18:46 -0400

    Update homebrew formula

--- Formula/bert-validator.rb
+++ Formula/bert-validator.rb
@@ -11,8 +11,8 @@
 class BertValidator < Formula
   desc "YAML-driven script validator"
   homepage "https://github.com/berttejeda/bert.validator"
-  url "https://github.com/berttejeda/bert.validator/archive/refs/tags/v1.5.0.tar.gz"
-  sha256 "27a413cffea41b9968d783c43f0b2ad4948b4c9be58693cc5eaa59df203e5941"
+  url "https://github.com/berttejeda/bert.validator/archive/refs/tags/v1.6.0.tar.gz"
+  sha256 "c9a5cd63173fc86532d0faaee386fae9caaca2d8928a2ab9bdd26842854322b6"
   license "MIT"
   head "https://github.com/berttejeda/bert.validator.git", branch: "main"
 
